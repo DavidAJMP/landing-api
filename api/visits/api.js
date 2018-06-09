fa5579e812b59585a6758b83aae2a7b9e50e3673
David Mendoza <albermen2209@gmail.com>
2018-06-08 22:20:44 -0500

    minimal changes

--- api/visits/api.js
+++ api/visits/api.js
@@ -5,31 +5,38 @@ let {
   await
 } = require('asyncawait');
 
- const all = (request, reply) => {
-    let key = request.params.key;
-    const getVisits = async(() => {
-      let campaign = await(models.Campaign.find({ where: { key } }));
-      if (campaign)
-        return await(models.Visit.findAll({ where: { CampaignId: campaign.id } }));
-      else
-        return [];
+const all = (request, reply) => {
+  let key = request.params.key;
+  const getVisits = async (() => {
+    let campaign = await (models.Campaign.find({
+      where: {
+        key
+      }
+    }));
+    if (campaign)
+      return await (models.Visit.findAll({
+        where: {
+          CampaignId: campaign.id
+        }
+      }));
+    else
+      return [];
+  });
+  getVisits()
+    .then((visits) => {
+      reply(visits).code(200);
     });
-    getVisits()
-      .then((visits) => {
-        reply(visits).code(200);
-      });
-  };
+};
 
-  const post = (request, reply) => {
-    let { visit } = request.payload;
-    let key = request.params.key;
-    const newVisit = async(() => {
-      let campaign = await(models.Campaign.find({ where: { key } }));
-      if (campaign) {
-        visit.CampaignId = campaign.id;
-        return await(models.Visit.create(visit));
-      } else {
-        return null;
+const post = (request, reply) => {
+  let {
+    visit
+  } = request.payload;
+  let key = request.params.key;
+  const newVisit = async (() => {
+    let campaign = await (models.Campaign.find({
+      where: {
+        key
       }
     }));
     if (campaign) {
@@ -39,6 +46,12 @@ let {
       return null;
     }
   });
+  if (campaign) {
+    visit.CampaignId = campaign.id;
+    return await (models.Visit.create(visit));
+  } else {
+    return null;
+  }
   newVisit()
     .then((visit) => {
       reply(visit).code(200);
@@ -46,16 +59,9 @@ let {
     .catch((error) => {
       reply(Boom.badImplementation());
     });
-    newVisit()
-      .then((visit) => {
-        reply(visit).code(200);
-      })
-      .catch((error) => {
-        reply(Boom.badImplementation());
-      });
-  };
+};
 
 module.exports = {
-    post,
-    all    
+  all,
+  post
 };
