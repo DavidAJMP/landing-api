49bda359e304d8c898438a73e2218fb9bd127552
dave-mp <davidajmp922@gmail.com>
2022-10-16 17:20:25 -0500

    Converted all modules to ES6

--- V2/db/campaign.js
+++ V2/db/campaign.js
@@ -1,25 +1,28 @@
-'use strict';
-module.exports = function(sequelize, DataTypes) {
-  var Campaign = sequelize.define('Campaign', {
-    key: {
-      type: DataTypes.STRING,
-      allowNull: false
+export default function (sequelize, DataTypes) {
+  var Campaign = sequelize.define(
+    "Campaign",
+    {
+      key: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      validThru: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
     },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    validThru: {
-      type: DataTypes.DATE,
-      allowNull: true
+    {
+      classMethods: {
+        associate: function (models) {
+          Campaign.hasMany(models.Visit);
+          Campaign.hasMany(models.Visitor);
+        },
+      },
     }
-  }, {
-    classMethods: {
-      associate: function(models) {
-        Campaign.hasMany(models.Visit);
-        Campaign.hasMany(models.Visitor);
-      }
-    }
-  });
+  );
   return Campaign;
-};
+}
--- V2/db/visitor.js
+++ V2/db/visitor.js
@@ -1,28 +1,31 @@
-'use strict';
-module.exports = function (sequelize, DataTypes) {
-  var Visitor = sequelize.define('Visitor', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: true
+export default function (sequelize, DataTypes) {
+  var Visitor = sequelize.define(
+    "Visitor",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      customData: {
+        type: DataTypes.BLOB("long"),
+        allowNull: true,
+      },
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    phone: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    customData: {
-      type: DataTypes.BLOB('long'),
-      allowNull: true
-    }
-  }, {
+    {
       classMethods: {
         associate: function (models) {
           Visitor.belongsTo(models.Campaign);
-        }
-      }
-    });
+        },
+      },
+    }
+  );
   return Visitor;
-};
+}
